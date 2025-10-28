86ff5dca8759114c452ff47481d17d711460862c
KarenAvila29 <avilamore29@gmail.com>
2025-10-27 20:20:02 -0500

    reto 2-creaccionTablas

---
+++ productosScript.sql
@@ -0,0 +1,9 @@
+-- creacion de tabla productos
+create table productos(
+	codigo int,
+	nombre varchar(50) not null,
+	descripcion varchar(200),
+	precio money not null,
+	stock int not null,
+	constraint productos_pk primary key (codigo)
+)
