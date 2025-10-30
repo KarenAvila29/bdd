cb49e9e4a0ca23a95ec31b519155901e5025fc5b
KarenAvila29 <avilamore29@gmail.com>
2025-10-29 19:06:28 -0500

    reto6-elima¿inar

---
+++ Eliminacion/estudiantesScript_Estudiante.sql
@@ -0,0 +1,58 @@
+--creacion tabla estudiantes
+
+create table estudiantes(
+	cedula char(10) not null,
+	nombre varchar(50) not null,
+	apellido varchar(50) not null,
+	email varchar(50) not null,
+	fecha_nacimiento date not null,
+	constraint estudiantes_pk primary key(cedula)
+)
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('1725478135','Maria','Perez','maria@gmail.com','20/10/2000')
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('1254785314','Rocio','Robles','rocio@gmail.com','20/10/2001')
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('2564781583','Karen','Avila','avila@gmail.com','20/10/1993')
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('5697452158','Belen','Guaman','belen@gmail.com','20/10/2002')
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('1725471452','Maritza','Aguilar','maritza@gmail.com','20/10/1995')
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('5741258412','Make','Paz','make@gmail.com','20/10/2000')
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('5478541254','Santa','Aguilar','santa@gmail.com','20/10/2000')
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('1725478854','Luli','Paz','luli@gmail.com','20/10/2000')
+
+select * from estudiantes
+
+--crear un select que traiga el nombre y cedula de los estudiantes
+select nombre,cedula from estudiantes
+
+--crear un select que traiga los nombres de todos los estudiantes cuya cedula empieze
+--con 17
+select nombre from estudiantes where cedula like '17%'
+
+--crear un select que traiga los nombres completos de todos los estudiantes cuyo nombre
+--empieze con A
+select nombre,apellido from estudiantes where nombre like 'A%'
+
+--RETO 5
+--Actualizar el apellido por Hernandez a todos los estudiantes que tengan una cedula 
+--que empiece por 17
+update estudiantes set apellido='Hernandez' where cedula like '17%'
+
+--RETO6
+--Elimina todos los estudiantes que tengan una cedula que termina por 05
+delete from estudiantes where cedula like '%05'
+
+
---
+++ Eliminacion/videojuegosScript_Eliminacion.sql
@@ -0,0 +1,44 @@
+--creacion de tabla videojuegos
+
+create table videojuegos(
+	codigo int not null, 
+	nombre varchar(100) not null,
+	descripcion varchar(300),
+	valoracion int not null,
+	constraint videojuegos_pk primary key (codigo)
+)
+
+insert into videojuegos(codigo,nombre,descripcion,valoracion)
+values('01','Super Mario','Juego para todas las edades',10)
+
+insert into videojuegos(codigo,nombre,descripcion,valoracion)
+values('02','Hollow Knight','Mayor de 18 años',10)
+
+insert into videojuegos(codigo,nombre,descripcion,valoracion)
+values('03','Ghost of Yotei','Juego para todas las edades',10)
+
+insert into videojuegos(codigo,nombre,descripcion,valoracion)
+values('04','Clair Obscur','Juego para todas las edades',10)
+
+insert into videojuegos(codigo,nombre,descripcion,valoracion)
+values('05','Super Mario','Diversion',10)
+
+select * from videojuegos
+
+--seleccionar todos los videojuegos con un nombre que comiencen con una C
+select * from videojuegos where nombre like 'C%'
+
+--seleccione video juegos con una valoracion entre 9 y 10
+select * from videojuegos where valoracion between '9' and '10'
+
+--seleccione video juegos con una descripcion null
+select * from videojuegos where descripcion is null
+
+--RETO5
+--Actualiza todas las decripciones de los videojuegoscuya valoracion sea mayor
+--que 9 por "Mejor puntuado"
+update videojuegos set descripcion ='Mejor puntuado' where valoracion > 9
+
+--RETO 6
+--Elimina los videojuegos cuya valoracion sea menor que 7
+delete from videojuegos where valoracion<7
